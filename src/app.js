77beda5612e8f02ce004ced6494c2788ef494ba4
CodeTitan77 <himanshutrivedi421@gmail.com>
2026-07-20 22:55:52 +0530

    Added Cors for the default fontend url

--- src/app.js
+++ src/app.js
@@ -5,10 +5,10 @@ import authRouter from "./routes/auth.routes.js";
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors({
-//     origin: "http://localhost:5173",
-//     credentials: true
-// }))
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 app.use(cors());
 app.use("api/auth",authRouter);
 
