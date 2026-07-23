e42274aaaae394b0834070a4952d81f8604f4c29
CodeTitan77 <himanshutrivedi421@gmail.com>
2026-07-23 23:00:05 +0530

    Updated packages

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import cookieParser from "cookie-parser";
 import express from "express";
 import cors from "cors";
 import authRouter from "./routes/auth.routes.js";
+import interviewRouter from "./routes/interview.routes.js";
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
@@ -9,8 +10,9 @@ app.use(cors({
     origin: "http://localhost:5173",
     credentials: true
 }))
-app.use(cors());
+
 app.use("api/auth",authRouter);
+app.use("api/interview",interviewRouter);
 
 export default app;
 
