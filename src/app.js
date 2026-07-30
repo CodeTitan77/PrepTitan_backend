52073fe7f9203cab1f4398db7b1e564d877b89c1
CodeTitan77 <himanshutrivedi421@gmail.com>
2026-07-30 23:40:17 +0530

    Added Puppeteer for resume generation updated the ai services

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import express from "express";
 import cors from "cors";
 import authRouter from "./routes/auth.routes.js";
 import interviewRouter from "./routes/interview.routes.js";
+
 const app=express();
 app.use(express.json());
 app.use(cookieParser());
