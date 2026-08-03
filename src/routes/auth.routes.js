3d831b30598c1f4250cf2cea264c18411b5212ea
CodeTitan77 <himanshutrivedi421@gmail.com>
2026-08-03 23:08:16 +0530

    Updated the authRoutes

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,13 +2,14 @@
 import express from "express";
 import authController from "../controllers/auth.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
+
 const Router=express.Router;
 const authRouter=Router();
 authRouter.post("/register",authController.registerController);
 authRouter.post("/login",authController.loginController);
 authRouter.get("/logout",authController.logoutController);
 authRouter.get("/get-me",authMiddleware.authUser,authController.getmeController);
-interviewRouter.get("/:interviewReportId/resume-pdf", authMiddleware.authUser, interviewController.generateResumePdfController)
+
 
 export default authRouter;
 
